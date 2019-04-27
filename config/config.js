456a8123f40ee565174ec910cc397fe522613c78
Luis Fuentes <fuentesld@gmail.com>
2019-04-27 09:09:09 -0500

    bug coneccion DB

--- config/config.js
+++ config/config.js
@@ -7,9 +7,10 @@ process.env.PORT = process.env.PORT || 3000;
 // =============================
 // Base de datos
 // =============================
+let urlDB;
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
-if ((process.env.NODE_ENV = "dev"))
+if (process.env.NODE_ENV === "dev")
   urlDB = "mongodb://localhost:27017/portalCautivo";
 else
   urlDB =
