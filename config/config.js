026995a1969427f3a28e375e6595b05645ad08a0
Luis Fuentes <fuentesld@gmail.com>
2019-04-27 09:34:46 -0500

    se oculto Uri de mongo en servidor produccion

--- config/config.js
+++ config/config.js
@@ -12,7 +12,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 if (process.env.NODE_ENV === "dev")
   urlDB = "mongodb://localhost:27017/portalCautivo";
-else
-  urlDB =
-    "mongodb://portal-admin:Jointics2019.!@ds149056.mlab.com:49056/portalcautivo";
+else urlDB = process.env.MONGO_URI;
 process.env.URLDB = urlDB;
