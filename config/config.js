4a2b2196cd230771a4fdfc9ee51927391d97e2cc
Luis Fuentes <fuentesld@gmail.com>
2019-04-28 07:44:50 -0500

    Googole sign-in

--- config/config.js
+++ config/config.js
@@ -24,3 +24,10 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 // SEED de autenticacion
 // =============================
 process.env.SEED = process.env.SEED || "secret";
+
+// =============================
+// Google Client
+// =============================
+process.env.CLIENT_ID =
+  process.env.CLIENT_ID ||
+  "861248453211-b4kup158r7fm14djabnhd2dr6ck4lbq4.apps.googleusercontent.com";
--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.CLIENT_ID);
 
 const Usuario = require("../models/usuario");
 const app = express();
@@ -18,7 +20,7 @@ app.post("/login", (req, res) => {
     if (!usuarioDB) {
       return res.status(400).json({
         ok: true,
-        err: { message: "(usuario) o contraseña incorrectos" }
+        err: { message: "usuario o contraseña incorrectos" }
       });
     }
 
@@ -38,6 +40,87 @@ app.post("/login", (req, res) => {
     );
 
     res.json({ ok: true, usuario: usuarioDB, token });
+  });
+});
+
+// Configuraciones de Google
+
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+
+  return {
+    nombre: payload.name,
+    email: payload.email,
+    img: payload.picture,
+    google: true
+  };
+  // const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+}
+// verify().catch(console.error);
+
+app.post("/google", async (req, res) => {
+  let token = req.body.idtoken;
+  let googleUser = await verify(token).catch(e => {
+    return res.status(403).json({ ok: false, err: e });
+  });
+
+  Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(500).json({ ok: false, err });
+    }
+
+    // El usuario existe
+    if (usuarioDB) {
+      // El usuario se dio de alta por sistema POST/usuario
+      if (usuarioDB.google === false)
+        return res.status(400).json({
+          ok: false,
+          err: { message: "Debe de usar su autenticacion normal" }
+        });
+      else {
+        // el usuario ya se dio de alta por google
+        // genera token
+        let token = jwt.sign(
+          {
+            usuario: usuarioDB
+          },
+          process.env.SEED,
+          { expiresIn: process.env.CADUCIDAD_TOKEN }
+        );
+        return res.json({ ok: true, usuario: usuarioDB, token });
+      }
+    } else {
+      // El usuario no se ha registrado antes (Nuevo usuario)
+      let usuario = new Usuario();
+      usuario.nombre = googleUser.nombre;
+      usuario.email = googleUser.email;
+      usuario.img = googleUser.img;
+      usuario.google = true;
+      usuario.password = ":)";
+
+      usuario.save({ err }, usuarioDB => {
+        if (err) {
+          return res.status(500).json({ ok: false, err });
+        }
+        // genera token
+        let token = jwt.sign(
+          {
+            usuario: usuarioDB
+          },
+          process.env.SEED,
+          { expiresIn: process.env.CADUCIDAD_TOKEN }
+        );
+        return res.json({ ok: true, usuario: usuarioDB, token });
+      });
+    }
   });
 });
 
